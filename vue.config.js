9e560822e874ff7e33d1f5f81c3c4b5f5a905b18
SAKKAARAT SUWANTAI <sakkarat.tai@gmail.com>
2026-04-14 18:12:36 +0700

    ปรับชื่อแท็บเว็บไซต์

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      // Browser tab title (used by public/index.html via htmlWebpackPlugin.options.title)
+      title: 'IT Asset Management',
+    },
+  },
 })
